23ae52f5cf3ae8005d9075e85a2f8cb92641dc9c
codezeros18 <125339231+codezeros18@users.noreply.github.com>
2024-10-29 11:58:23 +0700

    heys

--- src/Pages/Note5.tsx
+++ src/Pages/Note5.tsx
@@ -1,10 +1,10 @@
-import video from '../assets/vid5.mp4';
+import video1 from '../assets/vid5.mp4';
 import AudioPlayer from '../Components/AudioPlayer';
 import Lyric from '../Components/Lyric';
-import {Link} from 'react-router-dom';
-import {FaAngleDown} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { FaAngleDown } from 'react-icons/fa';
 
-const Note5 = () => {  
+const Note1 = () => {  
   return (
     <>
         <div className="relative w-full min-h-[90vh] overflow-hidden">
@@ -15,28 +15,33 @@ const Note5 = () => {
                     </Link>
                 </button>
             </div>
-            <video className="absolute top-0 left-0 w-full h-full object-cover"
-                src={video}
-                autoPlay
-                muted
-                loop> 
+            {/* Background video container */}
+            <div 
+                className="absolute top-0 left-0 w-full h-full bg-cover bg-center bg-no-repeat" 
+                style={{
+                    backgroundImage: `url(${video1})`, 
+                    backgroundSize: "cover", 
+                    backgroundPosition: "center",
+                    opacity: 0.9,
+                }}
+            >
                 <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-90" />
-            </video>
-            <div className="absolute bottom-24 left-0 w-full p-4">
+            </div>
+            
+            <div className="absolute bottom-24 left-0 w-full p-4 z-20">
                 <div className="px-4 py-4">
                     <h2 className="text-[18px] font-[600] text-white">I Like Me Better</h2>
                     <h4 className="text-[16px] text-white text-opacity-50">LINTANG</h4>
                 </div>
             </div>
-            <AudioPlayer/>
             
+            <AudioPlayer />
         </div>
         <div className='mb-60'>
-
-        <Lyric/>
+            <Lyric />
         </div>
     </>
   )
 }
 
-export default Note5;
+export default Note1;
